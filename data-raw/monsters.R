0dfcdfa611afceaeb67b283daee245cf9f3d4ffb
Jarad Niemi <niemi@iastate.edu>
2023-10-02 21:06:42 -0500

    Add names to monsters

--- data-raw/monsters.R
+++ data-raw/monsters.R
@@ -1,19 +1,30 @@
-library(dplyr)
+library("readr")
+library("dplyr")
+library("stringr")
+library("forcats")
 
 monsters <- readr::read_csv("monsters.csv") %>%
-  select(-url)
-
-names(monsters) <- tolower(names(monsters))
-names(monsters) <- gsub(" ", "_", names(monsters))
-
-# clean up
-monsters <- monsters %>%
+  dplyr::mutate(name = stringr::str_extract(url, "Monsters:\\s*(.*?)\\s*#"),
+                name = gsub("Monsters:", "", name),
+                name = gsub("#", "", name),
+                name = gsub("%20", " ", name),
+                name = gsub("%27", "'", name)
+                ) %>%
+  
+  rename_with(tolower) %>%
   
   # Fix challenge rating
-  mutate(challenge_rating = as.numeric(plyr::revalue(challenge_rating,
-                                                     c("1/2"=0.5, 
-                                                       "1/4"=0.25, 
-                                                       "1/8"=0.125))))
+  dplyr::mutate(`challenge rating` = dplyr::recode(`challenge rating`,
+                                                 `1/2` = "0.5", 
+                                                 `1/4` = "0.25", 
+                                                 `1/8` = "0.125"),
+                `challenge rating` = as.numeric(`challenge rating`)) %>%
   
-devtools::use_data(monsters, overwrite = TRUE)
+  dplyr::select(-url) %>%
+  
+  dplyr::select(name, size, type, 
+         `challenge rating`, ac, hp, str, dex, con, int, wis, cha,
+         dplyr::everything())
+
+usethis::use_data(monsters, overwrite = TRUE)
 
